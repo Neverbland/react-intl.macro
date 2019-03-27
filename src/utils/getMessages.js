1496ad8bc831f30b1ecdc7f26c9f05abf3cbbf75
Tom Allen <tomjamesallenweb@gmail.com>
2019-03-27 17:15:51 +0000

    Adds logic to getMessage method to collect the properties that we care about in the case that objectExpressionToObject is unable to correctly parse the object

--- src/utils/getMessages.js
+++ src/utils/getMessages.js
@@ -7,9 +7,35 @@ export default function getMessages(
   referencePath: BabelPath,
 ): Array<MessageDescriptor> {
   const properties = referencePath.parentPath.get('arguments.0.properties');
-  const messages = properties
-    .map(property => property.get('value'))
-    .map(objectExpressionToObject);
+  const messages = properties.map(property => {
+    // If we have an id here then the object has been correctly parsed.
+    const value = objectExpressionToObject(property.get('value'));
+    if (value.id) {
+      return value;
+    }
+
+    // Otherwise we need to manually gather the properties that we care about.
+    const id = property.node.value.properties.find(p => p.key.value === 'id');
+    const defaultMessage = property.node.value.properties.find(
+      p => p.key.value === 'defaultMessage',
+    );
+    const description = property.node.value.properties.find(
+      p => p.key.value === 'description',
+    );
+
+    const message = {};
+    if (id && id.value && id.value.value) {
+      message.id = id.value.value;
+    }
+    if (defaultMessage && defaultMessage.value && defaultMessage.value.value) {
+      message.defaultMessage = defaultMessage.value.value;
+    }
+    if (description && description.value && description.value.value) {
+      message.description = description.value.value;
+    }
+
+    return message;
+  });
 
   return messages;
 }
